7cd114f251f8e463d0d6df82481be46b1a936c3d
C-Soup <152320714+C-Soup@users.noreply.github.com>
2023-11-28 19:04:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+UpdIntID = setInterval(changeTime, 100);
+const messages = ["liebenswert", "bewundernswert", "atemberaubend", "einzigartig", "beeindruckend", "charmant", "attraktiv", "entzückend", "klug", "fröhlich", "fantastisch", "großartig", "strahlend", "mitreißend", "selbstbewusst", "erfolgreich", "stark", "hilfsbereit", "talentiert", "umwerfend", "überragend", "unwiderstehlich", "warmherzig", "wertvoll", "25", "26", "27", "28"];
+let lockRefresh = False;
+
+function changeTime(){
+
+    let date = new Date();
+
+    let day = 24;
+    let hrs = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds(); 
+
+    document.getElementById("sec").innerHTML = (59 - secs).toString();
+    document.getElementById("min").innerHTML = (59 - mins).toString();   
+    document.getElementById("hrs").innerHTML = (23 - hrs).toString();
+    document.getElementById("picture").src = ("pictures/" + "pic" + day.toString() + ".jpeg");
+    document.getElementById("message").innerHTML = ("Sascha, du bist " + messages[(day - 1)] + "!");
+    document.getElementById("christmascountdown").innerHTML = "... und noch " + ((24 - date.getDate()).toString()) + " Tage bis Weihnachten";
+    document.getElementById("test").innerHTML = ("pictures/" + "pic" + day.toString() + ".jpeg")
+    /*
+    if (hrs == 0 && lockRefresh == False) {
+        document.getElementById("picture").src = ("pictures/" + date.getDate() + ".png");
+        document.getElementById("test").innerHTML = ("Sascha, du bist " + messages[(date.getDate() - 1)]);
+        lockRefresh = True;
+    } else if (lockRefresh = True) {
+        lockRefresh = False;
+    }
+    */
+}
+
+/*changeImgURL(getDate())*/
